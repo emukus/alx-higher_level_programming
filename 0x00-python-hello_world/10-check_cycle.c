8d17576b50197bc7e5e26c5eddd45010e9111bc7
emukus <mukuna.eugene@gmail.com>
2022-06-27 20:16:25 +0000

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,12 +1,13 @@
 #include "lists.h"
 
 /**
-* check_cycle - function that checks if a singly linked list has a
-* cycle in it
-* based on Floyd's cycle-finding algorithm
-* @list: ptr to the struct listint_t
-* Return: 0 if there is no cycle, 1 if there is a cycle
-*/
+ * check_cycle - function that checks if a singly linked list has
+ * a cycle in it
+ * based on Floyd's cycle-finding algorithm
+ * @list: pointer to the struct listint_t
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
 
 int check_cycle(listint_t *list)
 {
