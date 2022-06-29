4ac9b4ef79ba8aa73b0c2d5c6d29012820c3b33c
emukus <mukuna.eugene@gmail.com>
2022-06-29 00:32:04 +0000

    Remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    """Function that creates a copy of the string without 
+    """Function that creates a copy of the string without
     the character at position n"""
     if n < 0:
         return (str)
