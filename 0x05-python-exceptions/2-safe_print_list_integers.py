d7bc3f1c33ab9b98d1ec658a14eb07c58ad1ef57
emukus <mukuna.eugene@gmail.com>
2022-07-18 18:08:49 +0000

    Print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        div = a / b
+    except ZeroDivisionError:
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return div
