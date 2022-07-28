f31ade326645715f63a1462c8e11c73abbb75c6b
emukus <mukuna.eugene@gmail.com>
2022-07-28 11:48:50 +0000

    CPython #3: Python Strings

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -24,7 +24,7 @@ void print_python_string(PyObject *p)
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
-		printf("  type: compacyt unicode object\n");
+		printf("  type: compact unicode object\n");
 	printf("  length: %ld\n", length);
 	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
