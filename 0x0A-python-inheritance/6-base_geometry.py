fb624129676d49ce55dbba71da2654ef2ebc5039
emukus <mukuna.eugene@gmail.com>
2022-08-01 22:32:27 +0000

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represents class BaseGeometry."""
+
+    def area(self):
+        """Function that raises not implemented exception."""
+        raise Exception("area() is not implemented")
