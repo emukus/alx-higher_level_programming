211c1f9e18da1ee2e9b3d94d2f663adf2a984400
emukus <mukuna.eugene@gmail.com>
2022-08-01 23:50:21 +0000

    Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -11,3 +11,12 @@ class Square(Rectangle):
         super().integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def area(self):
+        """Computes the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Returns str representation of an instance of class Square"""
+        return "[{}] {}/{}".format(type(self).__name__, self.__size,
+                                   self.__size)
