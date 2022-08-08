2aeedbb344b1be644bbac3f7701dc14045f5a175
emukus <mukuna.eugene@gmail.com>
2022-08-08 20:05:58 +0000

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-"""Defines a class Square."""
+"""
+This module implements a Square object
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square that inherits from class Rectangle"""
+    """Square implementation"""
 
     def __init__(self, size: int, x=0, y=0, id=None):
-        """Initializes a new Square"""
+        """initialization
+        """
         super().__init__(size, size, x, y, id)
         self.__size = size
 
     @property
     def size(self) -> int:
-        """Size getter
-        Returns the size(side) of the square
+        """size getter
         """
         return self.__size
 
     @size.setter
     def size(self, value: int):
-        """Size setter
-        Sets the size of the square
+        """size setter
         """
         self.__size = value
         self.width = self.height = value
 
     def __str__(self) -> str:
-        """The __str__ method returns a string representation of the object
-        return: The id, x, y, and size of the square."""
+        """string representation"""
         id = self.id
         size = self.__size
         x = self.x
@@ -36,8 +36,7 @@ class Square(Rectangle):
         return "[Square] ({}) {}/{} - {}".format(id, x, y, size)
 
     def update(self, *args, **kwargs):
-        """Updates square arguments
-        """
+        """update arguments"""
         attr = ['id', 'size', 'x', 'y']
         if args:
             for at, numb in zip(attr, args):
@@ -47,9 +46,10 @@ class Square(Rectangle):
                 if key in attr:
                     setattr(self, key, value)
 
-    def to_dictionary(self):
-        """Square to dictionary
-        Returns a dictionary representation of a Square instance
-        return: A dictionary with id, size, x, and y of the square
-        """
-        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+    def to_dictionary(self) -> dict:
+        """square to dictionary"""
+        id = self.id
+        size = self.__size
+        x = self.x
+        y = self.y
+        return {'id': id, 'x': x, 'size': size, 'y': y}
