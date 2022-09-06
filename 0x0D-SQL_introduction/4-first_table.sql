fb266ea5d158a98abbfa99b1b4ed2ae54994f062
emukus <mukuna.eugene@gmail.com>
2022-09-06 13:28:21 +0000

    First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+--  Creates a table called first_table in the current database in your MySQL server with values id and name.
+-- If the table first_table already exists, your script should not fail.
+-- not allowed to use the SELECT or SHOW statements.
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
