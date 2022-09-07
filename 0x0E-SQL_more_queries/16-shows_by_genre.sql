c4f809aaad7d84f0ca011d2e4e0313dcdc701566
emukus <mukuna.eugene@gmail.com>
2022-09-07 23:24:50 +0000

    List shows and genres

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,6 +5,8 @@
 -- You can use only one SELECT statement
 SELECT s.title, g.name
 FROM tv_shows s
-LEFT JOIN tv_show_genres m ON s.id = m.show_id
-LEFT JOIN tv_genres g ON m.genre_id = g.id
-ORDER BY s.title ASC;
+LEFT JOIN tv_show_genres m
+ON s.id = m.show_id
+LEFT JOIN tv_genres g
+ON m.genre_id = g.id
+ORDER BY s.title, g.name ASC;
